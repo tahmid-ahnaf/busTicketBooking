a0df83fd646f178ce69c3da3e1d970f91bd814ce
AhnafTahmiddie <ahnaftahmid2604@gmail.com>
2024-02-19 02:07:21 +0600

    navbar added price calculation remains hero added

--- app.js
+++ app.js
@@ -11,14 +11,15 @@
 const seatButtonsContainer = document.getElementById("seatButtonsContainer");
 let seatButtons = document.getElementsByClassName("btn");
 let selectedButtonsCount = 0;
-// console.log(seatButtons);
+
 
 for (let i = 0; i < seatButtons.length; i++) {
     seatButtons[i].addEventListener("click", function() {
       if (selectedButtonsCount < 4 && !seatButtons[i].classList.contains("bg-[#1DD100]")) {
         seatButtons[i].classList.add("text-white", "bg-[#1DD100]");
         selectedButtonsCount++;
-        console.log(seatButtons[i].innerText);
+
+        document.getElementById("seatCount").innerText = selectedButtonsCount;
         addTableData("seatTable", seatButtons[i].innerText);
 
         totalPrice = parseInt(document.getElementById("totalPrice").innerText)
@@ -26,18 +27,31 @@ for (let i = 0; i < seatButtons.length; i++) {
         
         document.getElementById("totalPrice").innerText = totalPrice;
 
+        grandTotalPrice = parseInt(document.getElementById("grandTotal").innerText)
+        grandTotalPrice = grandTotalPrice + 550;
+        
+        document.getElementById("grandTotal").innerText = grandTotalPrice;
 
-      } else {
+
+      }
+       
+      else {
+
         if(!seatButtons[i].classList.contains("bg-[#1DD100]"))
         {
             document.getElementById("my_modal_1").showModal()
-        }
-        
 
-        
+        }
       }
+
+      if (selectedButtonsCount == 4) {
+   
+        removeAttributeById("couponInput", "disabled");
+        removeAttributeById("couponApplyBtn", "disabled");
+    }
     });
   }
+
 
 
 function addTableData(tableId, seat)
@@ -54,4 +68,78 @@ function addTableData(tableId, seat)
         row.appendChild(seatCategory);
         row.appendChild(seatPrice);
         table.querySelector('tbody').appendChild(row);
+}
+
+function removeAttributeById(id, attrName)
+{
+  document.getElementById(id).removeAttribute(attrName);
+}
+
+
+document.getElementById("couponApplyBtn").addEventListener("click", function(event){
+  let couponValue = document.getElementById("couponInput").value;
+  if(couponValue==="NEW15")
+  {
+    couponHandle(15);
+  }
+
+  else if(couponValue === "Couple 20")
+  {
+    couponHandle(20);
+  }
+
+  else
+  {
+    // document.getElementById("my_modal_1").classList.add("hidden")
+    document.getElementById("my_modal_2").showModal()
+  }
+})
+
+
+document.getElementById("nextBtn").addEventListener("click", function(event){
+
+    document.getElementById("my_modal_3").showModal()
+})
+
+
+function createDivElement(containerName, discount)
+{
+
+    let containerDiv = document.getElementById(containerName);
+    let reference = containerDiv.children[3];
+    let divNew = document.createElement("div");
+    let pNew = document.createElement("p");
+    let pNew2 = document.createElement("p");
+    pNew.textContent = "Total Discount";
+    pNew2.textContent = "BDT " + discount;
+    divNew.appendChild(pNew);
+    divNew.appendChild(pNew2);
+    containerDiv.insertBefore(divNew,reference);
+    divNew.classList.add("flex", "justify-between");
+    pNew.classList.add("font-inter", "font-semibold");
+    pNew2.classList.add("font-inter", "font-semibold");
+
+}
+
+
+function couponHandle(discountPercentage)
+{
+
+    document.getElementById("couponForm").classList.add("hidden");
+    totalPrice = parseInt(document.getElementById("totalPrice").innerText)
+    discount = totalPrice * discountPercentage * 0.01;
+
+    grandTotalPrice = parseInt(document.getElementById("grandTotal").innerText)
+    grandTotalPrice = grandTotalPrice - grandTotalPrice*discountPercentage*0.01;
+        
+    document.getElementById("grandTotal").innerText = grandTotalPrice;
+    createDivElement("priceContainer", discount);
+
+}
+
+function scrollToSection(sectionId) {
+  var section = document.getElementById(sectionId);
+  if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+  }
 }
